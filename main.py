63e1c026f557faded0db1bfb2e078ad9cc19696d
rmnzm <romzamaraev@yandex.ru>
2022-04-24 08:49:53 +0300

    module-2 / inf about json and parsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+payload = {"name": "User"}
+response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
 print(response.text)
 
