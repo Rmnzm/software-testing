7b0acd0067a6de870c76063dd83758f7f9fcb0e2
Rmnzm <romzamaraev@yandex.ru>
2022-04-24 16:39:38 +0300

    module-2 / requests practice

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# response = requests.get('https://playground.learnqa.ru/api/get_text', params={"param1":1})
+#
+#
+# try:
+#     parsed_response_json = response.json()
+#     print(parsed_response_json)
+# except JSONDecodeError:
+#     print("Invalid format")
+
+response = requests.post("https://playground.learnqa.ru/api/check_type", data={"param": "param"})
 print(response.text)
 
