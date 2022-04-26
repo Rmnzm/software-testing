61755cf216152784b409334a0733f6a948515ac8
rmnzm <romzamaraev@yandex.ru>
2022-04-26 16:44:39 +0300

    module-2 /

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
 
 payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-print(response.text)
+
+# REDIRECTING 301 status code
+
+# response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+# first_response = response.history[0]
+# second_response = response
+# print(first_response.url)
+# print(second_response.url)
+#
+# print(response.status_code)
 
