6533557ba37d7c79e15cf6ab328a459c5d6c0530
rmnzm <romzamaraev@yandex.ru>
2022-04-26 16:54:58 +0300

    main

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from json.decoder import JSONDecodeError
 import requests
 
-# response = requests.get('https://playground.learnqa.ru/api/get_text', params={"param1":1})
-#
-#
-# try:
-#     parsed_response_json = response.json()
-#     print(parsed_response_json)
-# except JSONDecodeError:
-#     print("Invalid format")
+payload = {"name": "User"}
 
-response = requests.post("https://playground.learnqa.ru/api/check_type", data={"param": "param"})
-print(response.text)
+# REDIRECTING 301 status code
 
+# response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+# first_response = response.history[0]
+# second_response = response
+# print(first_response.url)
+# print(second_response.url)
+#
+# print(response.status_code)
+
+# HEADERS
+# headers = {"some_header": "123"} setUp USER headers
+# response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers= headers)  improve headers
+#
+# print(response.text) in this you may see your headers
+# print(response.headers)
