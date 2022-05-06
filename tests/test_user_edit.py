ef6b0b9221af0d3ddd1d47fb4d243007e9727ab7
rzamaraev <rzamaraev@fix.ru>
2022-05-06 12:31:18 +0200

    homework exercise 17 tests edit users

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -52,3 +52,112 @@ class TestUserEdit(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
+
+    def test_edit_user_from_unauthorised_user(self):
+        new_name = "Changed Name"
+
+        response_edit = MyRequests.put(
+            "/user/2",
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response_edit, 400)
+        assert response_edit.content.decode('utf-8') == "Auth token not supplied"
+
+    def test_edit_user_email_to_incorrect_email(self):
+        register_data = self.prepare_registration_user()
+        response_resister = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response_resister, 200)
+        Assertions.assert_json_has_key(response_resister, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response_resister, "id")
+
+        # LOGIN
+        login_data = {
+            "email": email,
+            "password": password
+        }
+        response_login = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response_login, "auth_sid")
+        token = self.get_header(response_login, "x-csrf-token")
+
+        new_email = "email example.com"
+
+        response_edit = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": new_email}
+        )
+
+        Assertions.assert_code_status(response_edit, 400)
+        assert response_edit.content.decode('utf-8') == "Invalid email format"
+
+    def test_edit_user_from_auth_another_user(self):
+        data_user_edit = self.prepare_registration_user()
+
+        response_user_edit = MyRequests.post("/user/", data=data_user_edit)
+
+        Assertions.assert_code_status(response_user_edit, 200)
+        Assertions.assert_json_has_key(response_user_edit, "id")
+        user_id = response_user_edit.json()["id"]
+
+        response1 = MyRequests.post("/user/login", data={
+            "email": "vinkotov@example.com",
+            "password": "1234"
+        })
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        new_name = "Changed Name"
+
+        response_edit = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response_edit, 400)
+        assert response_edit.content.decode('utf-8') == "Please, do not edit test users with ID 1, 2, 3, 4 or 5."
+
+    def test_edit_username_to_shortest_name(self):
+        # REGISTER
+        register_data = self.prepare_registration_user()
+        response_resister = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response_resister, 200)
+        Assertions.assert_json_has_key(response_resister, "id")
+
+        email = register_data["email"]
+        first_name = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response_resister, "id")
+
+        # LOGIN
+        login_data = {
+            "email": email,
+            "password": password
+        }
+        response_login = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response_login, "auth_sid")
+        token = self.get_header(response_login, "x-csrf-token")
+
+        # EDIT
+        new_name = "a"
+
+        response_edit = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response_edit, 400)
+        assert response_edit.json()["error"] == "Too short value for field firstName"
